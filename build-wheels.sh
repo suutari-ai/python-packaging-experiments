8115df225554a68023611ead80979e8411d0ddd5
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-11-02 13:04:18 +0200

    Add build-wheels script

---
+++ build-wheels.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+wheel_dir=$(cd wheels && pwd)
+
+rm -f "$wheel_dir"/*.whl
+
+for d in jepulis jepulis-ai lisaosa-jepulikseen; do
+    pushd $d
+    rm -frv dist build *.egg-info
+    python setup.py sdist bdist_wheel
+    mv -v dist/*.whl "$wheel_dir"
+    popd
+done
